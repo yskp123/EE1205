87c68423f764f9fd39576663343a7788ba41fddd
yskp123 <145852027+yskp123@users.noreply.github.com>
2024-02-21 15:24:46 +0530

    Add files via upload

---
+++ GATE/ch_58_2022/codes/plot_lk.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+import os
+
+tau = 1
+K = 0.5
+# Define the transfer function coefficients
+num = [K*(tau**2),2*tau*K,K-(K**3)]
+den = [tau**3, 3*(tau**2), 3*tau-2*(K**2)*tau,1-2*(K**2)]
+
+# Create the transfer function H(s) = num(s) / den(s)
+sys = signal.TransferFunction(num, den)
+
+# Generate frequency response
+w, mag, phase = signal.bode(sys)
+
+log_w = np.log(w)
+
+# Plot magnitude and phase responses
+plt.figure()
+plt.subplot(2, 1, 1)
+plt.plot(log_w, mag)
+plt.grid(True)
+plt.xlabel(r'$\log(\omega)$')
+plt.ylabel(r'$|T(j\omega)|$')
+
+plt.subplot(2, 1, 2)
+plt.plot(log_w, phase)
+plt.grid(True)
+plt.xlabel(r'$\log(\omega)$')
+plt.ylabel(r'$\angle T(j\omega)$')
+
+plt.tight_layout()
+plt.savefig('../figs/plot_lk.png')
+os.system('open ../figs/plot_lk.png')
